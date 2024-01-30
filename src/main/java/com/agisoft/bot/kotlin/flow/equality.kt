c43bc319403878ea66b2233f3e99ba1829388881
kris <kris979@gmail.com>
2024-01-30 19:10:21 +0000

    playing with Kotlin

--- src/main/java/com/agisoft/bot/kotlin/flow/equality.kt
+++ src/main/java/com/agisoft/bot/kotlin/flow/equality.kt
@@ -1,5 +1,15 @@
 package com.agisoft.bot.kotlin.flow
 
+fun max(a: Int, b: Int): Int {
+    if (a > b)
+        return a
+    else
+        return b
+}
+
+//There is no ternary operator condition ? then : else in Kotlin. Instead, if may be used as an expression:
+fun max2(a: Int, b: Int) = if (a > b) a else b
+
 fun main() {
     //Kotlin uses == for structural comparison and === for referential comparison.
     val authors = setOf("Shakespeare", "Hemingway", "Twain")
@@ -7,4 +17,6 @@ fun main() {
 
     println(authors == writers)
     println(authors === writers)
+
+    println(max(99, -42))
 }
